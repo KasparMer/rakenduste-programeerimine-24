4f8d13012cbe0c1fcbb5dd5eb6c1abef78be3a53
donKapu <merimimm@tlu.ee>
2024-10-06 21:06:22 +0300

    Validator add

--- 2024-09-23/Tsunami/backend/routes/todos.routes.js
+++ 2024-09-23/Tsunami/backend/routes/todos.routes.js
@@ -1,10 +1,27 @@
 const express = require("express");
+const { check, validationResult } = require("express-validator");
 const router = express.Router();
 const todosController = require("../controllers/todos.controller");
 
+const validateTodo = [
+  check("title").notEmpty().withMessage("Title is required"),
+  check("priority")
+    .isInt({ min: 1, max: 5 })
+    .withMessage("Priority must be between 1 and 5"),
+];
+
 router.get("/", todosController.read);
-router.post("/", todosController.create);
+
+router.post("/", validateTodo, (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  todosController.create(req, res, next);
+});
+
 router.put("/", todosController.update);
+
 router.delete("/", todosController.delete);
 
 module.exports = router;
