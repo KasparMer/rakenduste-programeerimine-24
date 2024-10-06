98416b742421f9ab8a95c416e8fa9257bc9cd627
donKapu <merimimm@tlu.ee>
2024-10-06 13:52:42 +0300

    Change

--- 2024-09-23/Tsunami/frontend/src/App.tsx
+++ 2024-09-23/Tsunami/frontend/src/App.tsx
@@ -1,33 +1,13 @@
-import React from "react";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import { CssBaseline } from "@mui/material";
+import "./App.css";
 import Cats from "./components/Cats";
 import Todos from "./components/Todos";
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#3f51b5",
-    },
-    secondary: {
-      main: "#f50057",
-    },
-    background: {
-      default: "#f0f0f0",
-    },
-  },
-  typography: {
-    fontFamily: "Roboto, sans-serif",
-  },
-});
-
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
+    <>
       <Cats />
       <Todos />
-    </ThemeProvider>
+    </>
   );
 }
 
